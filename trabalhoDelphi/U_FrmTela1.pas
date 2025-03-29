4cc06f9c30e063c813c01a4b72916a7a47f3aeb9
Marcos da Luz <108414450+NerusNexuss@users.noreply.github.com>
2025-03-29 14:03:21 -0300

    .
    .

---
+++ trabalhoDelphi/U_FrmTela1.pas
@@ -0,0 +1,68 @@
+unit U_FrmTela1;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.StdCtrls;
+
+type
+  TFrmTela1 = class(TForm)
+    BtnCalcular: TButton;
+    LbAltura: TLabel;
+    LbPeso: TLabel;
+    EdtAltura: TEdit;
+    EdtPeso: TEdit;
+    LbIMC: TLabel;
+    LbClassificacao: TLabel;
+    procedure BtnCalcularClick(Sender: TObject);
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  FrmTela1: TFrmTela1;
+
+implementation
+
+{$R *.dfm}
+
+procedure TFrmTela1.BtnCalcularClick(Sender: TObject);
+var
+  Peso,Altura, IMC: Double;
+  Classificacao: String;
+begin
+  Peso := StrToFloatDef(EdtPeso.Text,0);
+  Altura := StrToFloatDef(EdtAltura.Text, 0);
+
+  if (Peso <= 0) or (Altura <= 0) then
+  begin
+   showMessage('Por favor inserir valores validos para o peso');
+   exit;
+  end;
+
+  IMC := Peso / (Altura * Altura);
+
+  if IMC < 18.5 then
+    Classificacao := 'Magreza'
+  else if IMC < 24.9 then
+    Classificacao := 'Normal'
+  else if IMC < 29.9 then
+    Classificacao := 'Sobrepeso'
+  else if IMC < 39.9 then
+    Classificacao := 'Obesidade'
+  else
+    Classificacao := 'Obesidade Grave';
+
+  // Exibe os resultados nas labels
+  LbIMC.Caption := 'IMC: ' + FormatFloat('0.00', IMC);
+  LbClassificacao.Caption := 'Classificação: ' + Classificacao;
+
+
+
+
+end;
+
+end.
